3ea3d92ef28b2f0b91dff50d23541226a85170fc
beyond <921790476@qq.com>
2020-03-02 11:28:10 +0800

    装饰者模式 重构代码

--- src/main/java/com/wenhao/designpatterns/v4/decoration/decorator/AbstractDecorator.java
+++ src/main/java/com/wenhao/designpatterns/v4/decoration/decorator/AbstractDecorator.java
@@ -3,6 +3,16 @@ package com.wenhao.designpatterns.v4.decoration.decorator;
 import com.wenhao.designpatterns.v4.decoration.component.GatewayComponent;
 
 public abstract class AbstractDecorator extends GatewayComponent {
+    GatewayComponent gatewayComponent;
 
-    public abstract void service();
+    public void service() {
+        if (gatewayComponent != null) {
+            gatewayComponent.service();
+        }
+    }
+
+    public void setGatewayComponent(GatewayComponent gatewayComponent) {
+        this.gatewayComponent = gatewayComponent;
+    }
+
 }
--- src/main/java/com/wenhao/designpatterns/v4/decoration/decorator/impl/LogDecorator.java
+++ src/main/java/com/wenhao/designpatterns/v4/decoration/decorator/impl/LogDecorator.java
@@ -1,23 +1,23 @@
 package com.wenhao.designpatterns.v4.decoration.decorator.impl;
 
-import com.wenhao.designpatterns.v4.decoration.component.GatewayComponent;
 import com.wenhao.designpatterns.v4.decoration.decorator.AbstractDecorator;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LogDecorator extends AbstractDecorator {
 
-    GatewayComponent gatewayComponent;
+    //GatewayComponent gatewayComponent;
 
     @Override
     public void service() {
-        if (gatewayComponent != null) {
+        /*if (gatewayComponent != null) {
             gatewayComponent.service();
-        }
+        }*/
+        super.service();
         log.info("第二步》》》》 日志打印");
     }
 
-    public void setGatewayComponent(GatewayComponent gatewayComponent) {
+    /*public void setGatewayComponent(GatewayComponent gatewayComponent) {
         this.gatewayComponent = gatewayComponent;
-    }
+    }*/
 }
