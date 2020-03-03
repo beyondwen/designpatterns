2ccb7f045e202293de5bcdd372479c7377c79584
wenhao <921790476@qq.com>
2020-03-04 01:08:52 +0800

    适配器模式

--- src/main/java/com/wenhao/designpatterns/v8/adapter/V8Test.java
+++ src/main/java/com/wenhao/designpatterns/v8/adapter/V8Test.java
@@ -1,8 +1,9 @@
 package com.wenhao.designpatterns.v8.adapter;
 
 import com.wenhao.designpatterns.v8.adapter.domain.LogBean;
-import com.wenhao.designpatterns.v8.adapter.service.LogWriteFileService;
-import com.wenhao.designpatterns.v8.adapter.service.impl.ClassAdapter;
+import com.wenhao.designpatterns.v8.adapter.service.LogWriteDbService;
+import com.wenhao.designpatterns.v8.adapter.service.impl.LogAdapter;
+import com.wenhao.designpatterns.v8.adapter.service.impl.LogWriteFileServiceImpl;
 
 public class V8Test {
 
@@ -14,10 +15,15 @@ public class V8Test {
         logBean.setLogId("111");
         logBean.setLogText("22222");
         logWriteDbService.writeDbFile(logBean);*/
-        LogWriteFileService logWriteDbService = new ClassAdapter();
+        /*LogWriteFileService logWriteDbService = new ClassAdapter();
         LogBean logBean = new LogBean();
         logBean.setLogId("111");
         logBean.setLogText("22222");
-        logWriteDbService.logWriteFile();
+        logWriteDbService.logWriteFile();*/
+        LogWriteDbService logWriteDbService = new LogAdapter(new LogWriteFileServiceImpl());
+        LogBean logBean = new LogBean();
+        logBean.setLogId("111");
+        logBean.setLogText("22222");
+        logWriteDbService.writeDbFile(logBean);
     }
 }
