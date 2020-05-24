082d70dc943b336764f2bd2af3e7d228583719bf
wenhao <921790476@qq.com>
2020-05-24 22:31:53 +0800

    装饰模式

--- src/main/java/com/wenhao/designpatterns/v12decoration/expand/V12_ExpandSevice.java
+++ src/main/java/com/wenhao/designpatterns/v12decoration/expand/V12_ExpandSevice.java
@@ -7,12 +7,21 @@ import com.wenhao.designpatterns.v12decoration.V12_CacheService;
  */
 public abstract class V12_ExpandSevice implements V12_CacheService {
 
+    // 传入 原始功能接口
     V12_CacheService v12_cacheService;
 
+    /**
+     * 调用新增功能时 同时 传入 原始接口 因为 调用新增功能 时 不得影响 原有功能 的调用
+     * @param v12_cacheService 原始接口
+     */
     public V12_ExpandSevice(V12_CacheService v12_cacheService) {
         this.v12_cacheService = v12_cacheService;
     }
 
+    /**
+     * 父类方法实现
+     * @return Object
+     */
     public Object get() {
         if (v12_cacheService != null) {
             return v12_cacheService.get();
